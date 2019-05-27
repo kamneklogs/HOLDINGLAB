e95ec30309c1ba39ff8fe1b7b240429baba5db61
kamneklogs <you@example.com>
2019-05-26 19:53:48 -0500

    "nkjsnkdjsjds"

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,7 +6,7 @@ public class Main {
     private Holding theHolding;
     
 public void init(){
-        //Creamos el mejor holding
+        //Creamos el mejor holdings
         
         theHolding = new Holding("El mejor holding");
         
