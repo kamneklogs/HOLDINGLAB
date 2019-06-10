92ac7409d2fa9abad9e9784e2e8ce10d8e5b6e9b
kamneklogs <you@example.com>
2019-06-10 00:35:51 -0500

    Documentation (Nothing)

--- src/model/VariousPC.java
+++ src/model/VariousPC.java
@@ -22,7 +22,7 @@ public class VariousPC extends LegalP implements TreeTaxes {
 
 	@Override
 	public String toString() {
-		return super.parcialToString() + "\n No. de productos:    " + products.size() + "\n" +"Impuesto aguaXarbol:     "+ waterXtree() + "\n";
+		return super.parcialToString() + "\nNo. de productos:    " + products.size() + "\n" +"Impuesto aguaXarbol:     "+ waterXtree() + "\n";
 	}
 
 	@Override
